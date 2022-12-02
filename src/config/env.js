25e1a69c4911315abed0c2d1cdd43607a248f63f
sarim hassan <sarim.hassan@whizzbridge.com>
2022-12-02 16:11:23 +0500

    API request urls updated

--- src/config/env.js
+++ src/config/env.js
@@ -12,35 +12,35 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 }
 
 const client = create({
-    baseURL,
+  baseURL,
 });
 
 export const config = async () => {
-    const token = localStorage.getItem('org-token');
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/json',
-        },
-    };
+  const token = localStorage.getItem('org-token');
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+    },
+  };
 };
 export const authConfig = async (token) => {
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'application/json',
-        },
-    };
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+    },
+  };
 };
 
 export const multipartConfig = async () => {
-    const token = localStorage.getItem('AC-Token');
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: 'multipart/form-data',
-        },
-    };
+  const token = localStorage.getItem('AC-Token');
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'multipart/form-data',
+    },
+  };
 };
 
 export default client;
