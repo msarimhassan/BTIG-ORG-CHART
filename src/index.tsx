fabf41f5d777d87f709e6e30f323b69c640757c4
Emmanuel Bioux <ebioux@btig.com>
2022-12-13 13:26:00 -0800

    app doesn't start anymore - trying a rollback

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,13 @@ import publicClientApplication from './configuration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <MsalProvider instance={publicClientApplication}>
-      <App />
-    </MsalProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <MsalProvider instance={publicClientApplication}>
+        <App />
+      </MsalProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
