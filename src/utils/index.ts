ae8a7d6ad5aa686873fa9386b3eb55dc0d21f032
Rasib Nadeem <rasib.nadeem12@thehexaa.com>
2022-12-13 10:21:54 +0500

    seqserver api token variable added in env

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,22 @@
-import { Network, Urls, config, SEQSERVER } from '../config';
-import seq from 'seq-logging';
+import { Network, Urls, config, SEQSERVERURL, SEQSERVERAPIKEY } from '../config'
+import seq from 'seq-logging'
 
 const logger = new seq.Logger({
-  serverUrl: SEQSERVER,
-  onError: (e) => console.log(e),
-});
+  serverUrl: SEQSERVERURL,
+  apiKey: SEQSERVERAPIKEY,
+  onError: e => console.log(e)
+})
 
 export const logMessage = (Message: any) => {
   logger.emit({
     timestamp: new Date(),
     level: 'Information',
-    messageTemplate: Message,
-  });
-};
+    messageTemplate: Message
+  })
+}
 
 export const AddNewMember = async (e: any, obj: any) => {
-  e.stopPropagation();
+  e.stopPropagation()
 
   const response = await Network.post(
     Urls.addMember(
@@ -30,19 +31,25 @@ export const AddNewMember = async (e: any, obj: any) => {
     (
       await config()
     ).headers
-  );
-  if (!response.ok) return alert(response.data.error);
-  logMessage('Added new Team');
-};
+  )
+  if (!response.ok) return alert(response.data.error)
+  logMessage('Added new Team')
+}
 
 export const checkKey = (key: any, user: any) => {
   if (key === 'displayName')
-    return Urls.updateDisplayName(user.userPrinicipalName, user.displayName);
+    return Urls.updateDisplayName(user.userPrinicipalName, user.displayName)
   else if (key === 'teamLead')
-    return Urls.updateTeamLeaderStatus(user.userPrinicipalName, user.teamLead);
+    return Urls.updateTeamLeaderStatus(user.userPrinicipalName, user.teamLead)
   else if (key === 'horizontal')
-    return Urls.updateHorizontalDimension(user.userPrinicipalName, user.horizontal);
-  else if (key === 'visible') return Urls.updateVisible(user.userPrinicipalName, user.visible);
-  else if (key === 'left') return Urls.updateLeftDimension(user.userPrinicipalName, user.left);
-  else if (key === 'Team') return Urls.updatePartofTeam(user.userPrinicipalName, user.reportsInto);
-};
+    return Urls.updateHorizontalDimension(
+      user.userPrinicipalName,
+      user.horizontal
+    )
+  else if (key === 'visible')
+    return Urls.updateVisible(user.userPrinicipalName, user.visible)
+  else if (key === 'left')
+    return Urls.updateLeftDimension(user.userPrinicipalName, user.left)
+  else if (key === 'Team')
+    return Urls.updatePartofTeam(user.userPrinicipalName, user.reportsInto)
+}
