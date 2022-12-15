6de5868dcf25c66e59b9650e63b4b6012a2631dc
Rosheni Malik <rmalik@btig.com>
2022-12-15 15:10:18 -0800

    Modified the navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,27 +3,33 @@ import './Navbar.css';
 import useAuth from '../../hooks/useAuth';
 import publicClientApplication from '../../configuration';
 import { logMessage } from '../../utils';
+import './bootstrap.css'
+import logo from './images/crosshair.svg'
 
 const Navbar: FC = () => {
-  const { token, setToken, activeUser } = useAuth();
+    const { token, setToken, activeUser } = useAuth();
 
-  const handleLogout = () => {
-    setToken(null);
-    localStorage.clear();
-    publicClientApplication.logoutPopup();
-    logMessage(`${activeUser.name} logout from the app`);
-  };
+    const handleLogout = () => {
+        setToken(null);
+        localStorage.clear();
+        publicClientApplication.logoutPopup();
+        logMessage(`${activeUser.name} logout from the app`);
+    };
 
-  return (
-    <div className='Navbar' data-testid='testnavbar'>
-      <h2 className='logo'>Org Chart</h2>
-      {token ? (
-        <button className='login-btn' onClick={() => handleLogout()}>
-          Logout
-        </button>
-      ) : null}
-    </div>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary" data-testid='testnavbar' >
+            <a className="navbar-brand" href=""><img alt="BTIG" src={logo} style={{ height: '50px', width: '50px', marginTop: '-1.25rem', marginLeft: '-1rem', marginBottom: '-.5rem' }} /></a>
+            <span className="navbar-text w-100">
+                Org Chart
+            </span>
+
+            {token ? (
+                <button className='login-btn' onClick={() => handleLogout()}>
+                    Logout
+                </button>
+            ) : null}
+        </nav >
+    )
 };
 
 export default Navbar;
