4d543036d72df73b3ee272bd14ce99e8380dce4d
Rosheni Malik <rmalik@btig.com>
2022-12-20 13:18:10 -0800

    Revert "Merge pull request #22 from BTIG/rm-aad-header"
    This reverts commit 4bb87ab7e4a8d825961e887390a0afc2400cc8b8, reversing
    changes made to 3c3aaddaa0e6179251d6be737f5f01ab868415ee.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,33 +3,27 @@ import './Navbar.css';
 import useAuth from '../../hooks/useAuth';
 import publicClientApplication from '../../configuration';
 import { logMessage } from '../../utils';
-import './bootstrap.css'
-import logo from './images/crosshair.svg'
 
 const Navbar: FC = () => {
-    const { token, setToken, activeUser } = useAuth();
+  const { token, setToken, activeUser } = useAuth();
 
-    const handleLogout = () => {
-        setToken(null);
-        localStorage.clear();
-        publicClientApplication.logoutPopup();
-        logMessage(`${activeUser.name} logout from the app`);
-    };
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.clear();
+    publicClientApplication.logoutPopup();
+    logMessage(`${activeUser.name} logout from the app`);
+  };
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary" data-testid='testnavbar' >
-            <a className="navbar-brand" href=""><img alt="BTIG" src={logo} style={{ height: '50px', width: '50px', marginTop: '-1.25rem', marginLeft: '-1rem', marginBottom: '-.5rem' }} /></a>
-            <span className="navbar-text w-100">
-                Org Chart
-            </span>
-
-            {token ? (
-                <button className='login-btn' onClick={() => handleLogout()}>
-                    Logout
-                </button>
-            ) : null}
-        </nav >
-    );
+  return (
+    <div className='Navbar' data-testid='testnavbar'>
+      <h2 className='logo'>Org Chart</h2>
+      {token ? (
+        <button className='login-btn' onClick={() => handleLogout()}>
+          Logout
+        </button>
+      ) : null}
+    </div>
+  );
 };
 
 export default Navbar;
